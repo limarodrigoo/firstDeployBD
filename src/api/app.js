506e4b5a9517132e457186f3e7e808e0c19caeb0
Rodrigo Lima <limarodrigoo@outlook.com>
2022-03-17 17:52:14 -0300

    add cors

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const rescue = require('express-rescue');
 const UserController = require('../controllers/user');
 const errorMiddleware = require('../middlewares/error');
 
 const app = express();
 
+app.use(cors())
+
 app.get('/users', rescue(UserController));
+
 
 app.use(errorMiddleware); 
 
